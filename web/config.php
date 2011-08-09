0e4e808f42052a9d1fd0f971d0405622358a0d7f
Matthew Ratzke <matthew.003@me.com>
2011-08-09 00:59:10 -0600

    final pagodabox test

--- web/config.php
+++ web/config.php
@@ -1,7 +1,5 @@
 <?php
 
-echo $_SERVER['HTTP_HOST'];
-
 if (!isset($_SERVER['HTTP_HOST'])) {
     die('This script cannot be run from the CLI. Run it from a browser.');
 }
@@ -9,6 +7,7 @@ if (!isset($_SERVER['HTTP_HOST'])) {
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     '127.0.0.1',
     '::1',
+    'airos.pagodabox.com',
 ))) {
     header('HTTP/1.0 403 Forbidden');
     die('This script is only accessible from localhost.');
